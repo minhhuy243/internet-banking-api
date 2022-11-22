ccad0c608770968c077f6397a8ff294fd00d6853
minhhuy243 <minhhuy243@gmail.com>
2022-11-22 16:10:08 +0700

    config auditor aware

---
+++ src/main/java/com/internetbanking/config/PersistenceConfig.java
@@ -0,0 +1,17 @@
+package com.internetbanking.config;
+
+import com.internetbanking.common.AppAuditorAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class PersistenceConfig {
+
+    @Bean
+    AuditorAware<String> auditorAware() {
+        return new AppAuditorAware();
+    }
+}
