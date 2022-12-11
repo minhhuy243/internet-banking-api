838aa008f8507e16c10fb7f8f445ac06c01c524e
minhhuy243 <minhhuy243@gmail.com>
2022-12-11 10:23:32 +0700

    config to deploy to Tomcat

--- src/main/java/com/internetbanking/InternetBankingApplication.java
+++ src/main/java/com/internetbanking/InternetBankingApplication.java
@@ -2,9 +2,10 @@ package com.internetbanking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InternetBankingApplication {
+public class InternetBankingApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InternetBankingApplication.class, args);
